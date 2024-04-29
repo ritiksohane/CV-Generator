6be42b9ff09a1064b93c7fd288e8085b2c9417bf
Ritik Sohane <112770296+ritiksohane@users.noreply.github.com>
2024-04-30 02:32:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import FullFun from './Full';
+
+function App() {
+  return (
+    <>
+      <nav>
+        <h1>CV Generator by Ritik</h1>
+      </nav>
+      <div className="App">
+        <FullFun />
+      </div>
+    </>
+  );
+}
+
+export default App;
